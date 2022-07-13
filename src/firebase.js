df508a697e90eb244fd6bf3833beb89d8f4e657e
observer1220 <observer1220@gmail.com>
2022-07-13 13:59:04 +0800

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/compat/database'
 const firebaseConfig = {
   apiKey: 'AIzaSyDe0tjRovcJScbZ0n2j9PG92SB_sfFH9oc',
   authDomain: 'arch-8e2b9.firebaseapp.com',
+  databaseURL: 'https://arch-8e2b9-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'arch-8e2b9',
   storageBucket: 'arch-8e2b9.appspot.com',
   messagingSenderId: '888558710381',
   appId: '1:888558710381:web:fad97e47222a0f45749964',
-  measurementId: 'G-9BRTLHRNTB',
-  databaseURL: 'https://arch-8e2b9-default-rtdb.asia-southeast1.firebasedatabase.app/'
+  measurementId: 'G-9BRTLHRNTB'
 }
 firebase.initializeApp(firebaseConfig)
 
